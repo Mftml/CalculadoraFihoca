9e22dc2742ef05fa8772b7207eb057cf196d63c4
PC0P9WC1\User <ves@saber.on>
2018-08-21 09:12:11 +0200

    Revert "cambio"
    This reverts commit f2b60d9845c147a6ca5b46f02e24851cd1935e40.

--- CalcuradoraPrivalia1/src/main/java/com/privalia/presentation/iCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.privalia.presentation;
-
-public interface iCalculator {
-	public int add(int num1, int num2);
-	public int substract(int num1, int num2);
-	public int multiply(int num1, int num2);
-	public int divide(int num1, int num2);
-
-}
